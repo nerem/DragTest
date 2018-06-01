3f0c7c818356d7d39e5b0c293883a47f69dfb979
Felix <felix.kammerlander@gmail.com>
2018-06-01 14:43:29 +0200

    Upload

---
+++ Assets/DragImageAdder.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragImageAdder : MonoBehaviour {
+
+
+
+	public GameObject dragPrefab;
+
+	static int count = 0;
+
+	public void OnClick()
+	{
+		Debug.Log("Clicked Button!");
+		GameObject go = Instantiate(dragPrefab, this.transform);
+		count++; 
+		go.transform.localPosition = new Vector3(92f, -count * 105f, 0f);
+
+		GetComponent<RectTransform>().sizeDelta += new Vector2(0, 100f);
+	}
+}
---
+++ Assets/ItemSlotData.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class ItemSlotData : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+	public int item { get; set; }
+	public int amount { get; set; }
+
+	void Awake()
+	{
+		this.item = 0;
+		this.amount = 0;
+	}
+
+	public void OnBeginDrag(PointerEventData eventData)
+	{
+		Debug.Log("DRAG START");
+	}
+
+	public void OnDrag(PointerEventData eventData)
+	{
+		Debug.Log("DRAGGING");
+	}
+
+	public void OnEndDrag(PointerEventData eventData)
+	{
+		Debug.Log("END DRAG");
+	}
+}
